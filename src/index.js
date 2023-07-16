c66c0c7c4b69c90d51e9f3d93c64f293bfd7f820
MrDark <sarfaraj0110@gmail.com>
2023-07-16 13:11:27 +0530

    updated template

--- src/index.js
+++ src/index.js
@@ -6,14 +6,15 @@ import { BrowserRouter } from "react-router-dom";
 import { store } from "./redux/store";
 import { Provider } from "react-redux";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { HelmetProvider } from "react-helmet-async";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
+  <HelmetProvider>
     <BrowserRouter>
       <Provider store={store}>
         <App />
       </Provider>
     </BrowserRouter>
-  </React.StrictMode>
+  </HelmetProvider>
 );
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,19 +1,27 @@
 import React from "react";
+import { Helmet } from "react-helmet-async";
 
 const Home = () => {
   return (
-    <div className="container py-3">
-      <h3 className="text-center">Basic React-Redux Template</h3>
-      <div className="px-5 my-4">
-        <h4>Basic Features</h4>
-        <ul className="list-group list-group-flush">
-          <li class="list-group-item">Bootstrap</li>
-          <li class="list-group-item">React-Router-Dom</li>
-          <li class="list-group-item">Redux-Toolkit</li>
-          <li class="list-group-item">React Icons</li>
-        </ul>
+    <>
+      <Helmet>
+        <title>This is Home Page</title>
+        <link rel="canonical" href="https://www.plazmacloud.com/" />
+      </Helmet>
+
+      <div className="container py-3">
+        <h3 className="text-center">Basic React-Redux Template</h3>
+        <div className="px-5 my-4">
+          <h4>Basic Features</h4>
+          <ul className="list-group list-group-flush">
+            <li class="list-group-item">Bootstrap</li>
+            <li class="list-group-item">React-Router-Dom</li>
+            <li class="list-group-item">Redux-Toolkit</li>
+            <li class="list-group-item">React Icons</li>
+          </ul>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
